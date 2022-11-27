8c488df6e824b976199e343d9cded685afc09714
Md Rezone Ahamed <rezoneprinceme@gmail.com>
2022-11-27 20:46:52 +0600

    isadmin api done

--- index.js
+++ index.js
@@ -99,6 +99,20 @@ const run = async () => {
       res.send(result);
     });
 
+    app.get("/users/admin/:email", async (req, res) => {
+      const email = req.params.email;
+      const query = { email: email };
+      const user = await usersCollection.findOne(query);
+      res.send({ isAdmin: user?.role === "admin" });
+    });
+
+    app.get("/users/seller/:email", async (req, res) => {
+      const email = req.params.email;
+      const query = { email: email };
+      const user = await usersCollection.findOne(query);
+      res.send({ isSeller: user?.role === "seller" });
+    });
+
     // categories
 
     app.get("/categories", async (req, res) => {
